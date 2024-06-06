81794e43d858d0ddc3ac475a151c7c415be546ed
Vishalvagadiya <vishalvagadiya08@gmail.com>
2024-06-06 12:22:27 +0530

    Day5

---
+++ day5/src/Component/Login_Signup.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import Swal from 'sweetalert2';
+
+function Login() {
+    const [state, setState] = useState("Signup");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [lpassword, setLpassword] = useState("");
+    const [arr, setArr] = useState([]);
+
+    const Signup = () => {
+        const obj = {
+            username: name,
+            email: email,
+            pass: password
+        };
+        setArr([...arr, obj]);
+        setName('');
+        setEmail('');
+        setPassword('');
+        setState("Login");
+    };
+
+    const handleLogin = () => {
+        const data = arr.filter((el) => el.email === loginEmail && el.pass === lpassword);
+        console.log(data);
+        if (data.length > 0) {
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Login SucessFull",
+                showConfirmButton: false,
+                timer: 1500
+              });
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Incorrect Information!",
+              });
+        }
+    };
+
+    return (
+        <div>
+            {state === "Signup" ? (
+                <div className="main">
+                    <p className="sign" align="center">Sign Up</p>
+                    <input className="un" type="text" align="center" placeholder="Username" value={name} onChange={(e) => setName(e.target.value)} />
+                    <input className="email" type="email" align="center" placeholder="Enter Your Email Id" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input className="pass" type="password" align="center" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <button className="submit" align="center" onClick={Signup}>Sign Up</button>
+                    <p className="forgot" align="center" onClick={() => setState("Login")}>
+                        Already Have an Account? Click to Login
+                    </p>
+                </div>
+            ) : (
+                <div className="main2">
+                    <p className="sign" align="center">Login</p>
+                    <input className="email" type="email" align="center" placeholder="Enter Your Email Id" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)}/>
+                    <input className="pass" type="password" align="center" placeholder="Password" value={lpassword} onChange={(e) => setLpassword(e.target.value)}/>
+                    <button className="submit" align="center" onClick={handleLogin}> Login </button>
+                    <p className="forgot" align="center" onClick={() => setState("Signup")}>
+                        New User? Click Here to Sign Up!
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Login;
