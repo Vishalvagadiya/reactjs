e2ae6ae8d1f369624c45ad55d0c2e3d815e55a0e
Vishalvagadiya <vishalvagadiya08@gmail.com>
2024-06-08 23:00:34 +0530

    counter

--- day6/src/Component/Counter.jsx
+++ day6/src/Component/Counter.jsx
@@ -18,7 +18,7 @@ constructor (props){
     }
     render(){
         return(
-        <div>
+        <div className="main">
             <h1>Counter</h1>
             <h1>{this.state.count}</h1>
             <button type="button" onClick={this.counter}>Increment</button>
